9b879451e57cebc804bf6d73100d3450230c67b5
hetvi2022 <137720330+hetvi2022@users.noreply.github.com>
2023-07-14 02:18:10 +0530

    Update index.py
    Add energy consumption calculation

--- src/index.py
+++ src/index.py
@@ -2,13 +2,27 @@ from fastapi import FastAPI
 import asyncio
 import concurrent.futures
 import math
+import os
+import time
+
+
+cpu_rapl_path = '/sys/class/powercap/intel-rapl/intel-rapl:0/energy_uj'
+start_time = 0
+initial_value = 0
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def greetings():
+    global start_time, initial_value
     
+    start_time = time.time()
+
+    with open(cpu_rapl_path, 'r') as file:
+        initial_value = int(file.read().strip())
+    return {"Measuring Started From Home Page"}
+
+
 def perform_math_calculation(number):
     t = number
 
@@ -32,10 +46,23 @@ async def calculate(number: int):
     with concurrent.futures.ThreadPoolExecutor() as pool:
         result = await loop.run_in_executor(pool, perform_math_calculation, number)
 
-    return {"result": result}
+    with open(cpu_rapl_path, 'r') as file:
+        final_value = int(file.read().strip())
+
+    # Record final timestamp
+    end_time = time.time()
+
+    energy_consumption = final_value - initial_value
+    execution_time = end_time - start_time
+
+
+    return {"result: ": result,
+            "Energy consumption (microjoules (uJ)): " : energy_consumption,
+            "Execution time (seconds): " : execution_time
+            }
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
-# You can test the server by sending a GET request to http://localhost:8000/calculate/{number} where {number} is the integer you want to perform the math calculation on. The server will return a JSON response with the result.
+# Test the server by sending a GET request to http://localhost:8000/calculate/{number} where {number} is the integer you want to perform the math calculation on. The server will return a JSON response with the result.

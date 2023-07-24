255e101c34f0bf211acb95d515802185852e3b07
hetvi2022 <137720330+hetvi2022@users.noreply.github.com>
2023-07-24 22:07:02 +0530

    Update index.py
    Remove load function: matrix

--- src/index.py
+++ src/index.py
@@ -9,40 +9,6 @@ app = FastAPI()
 def greetings():
     return {"Home Page"}
 
-
-@app.get("/mm")
-async def matrix_multiplication(matrix1, matrix2):
-    # Example matrices
-    matrix1 = [
-        [1, 2, 3],
-        [4, 5, 6],
-    ]
-
-    matrix2 = [
-        [7, 8],
-        [9, 10],
-        [11, 12],
-    ]
-    # Check if matrices can be multiplied
-    if len(matrix1[0]) != len(matrix2):
-        raise ValueError("Matrices cannot be multiplied. Inner dimensions must match.")
-
-    # Initialize the result matrix with zeros
-    result = [[0 for i in range(len(matrix2[0]))] for j in range(len(matrix1))]
-
-    # Perform matrix multiplication
-    for i in range(len(matrix1)):
-        for j in range(len(matrix2[0])):
-            for k in range(len(matrix2)):
-                result[i][j] += matrix1[i][k] * matrix2[k][j]
-    
-    print(result)
-    
-    for row in result:
-        print(row)
-    
-    return json.dumps(matrix)
-
 def perform_math_calculation(number):
     t = number
 
